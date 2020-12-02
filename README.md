a8c8c161a4c33de0f72cbace719d838322e7aad1
Kashyap Patel <patel.kashyap226@gmail.com>
2020-12-01 21:49:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Scotiabank Nasdaq Index Fund Price Tracker
+
+An overnight cron job run by github action's that emails me everyday price of Scotiabank Nasdaq Index Fund mutual fund. Matual fund prices are only updated end of the day, which makes it harder to keep track of the price.
---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
+
+const selector = '.bns--fund-price .c--title';
+const url =
+    'https://www.scotiafunds.com/scotiafunds/en/fund-overviews/index-funds/scotia-nasdaq-index-fund.html';
+
+(async () => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.goto(url);
+        await page.waitForSelector(selector, { visible: true });
+
+        const element = await page.$(selector);
+        const price = await page.evaluate((el) => el.textContent, element);
+
+        await browser.close();
+
+        const uname = process.args[2];
+        const pass = process.args[3];
+        const to = process.args[4];
+
+        const transporter = nodemailer.createTransport({
+            secure: true,
+            service: 'gmail',
+            auth: { user: uname, pass: pass },
+        });
+
+        await transporter.sendMail({
+            from: uname,
+            to: to,
+            subject: 'Scotia Nasdaq Index Fund Price',
+            text: price,
+        });
+    } catch (e) {
+        console.error('Error: ', e);
+    }
+})();
